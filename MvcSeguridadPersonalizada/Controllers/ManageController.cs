2701b07b7a6afe7444b3bbcdd2656e2af5452ec1
AlexandruTajamar21 <alexandru.preutesei@tajamar365.com>
2022-02-08 13:51:49 +0100

    Add project files.

---
+++ MvcSeguridadPersonalizada/Controllers/ManageController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MvcSeguridadPersonalizada.Controllers
+{
+    public class ManageController : Controller
+    {
+
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if(username.ToLower() == "admin" && password.ToLower() == "admin")
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                Claim claimUserName = new Claim(ClaimTypes.Name,username);
+                identity.AddClaim(claimUserName);
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal,new AuthenticationProperties 
+                { 
+                    ExpiresUtc = DateTime.Now.AddMinutes(15)
+                });
+                return RedirectToAction("Perfil","Usuario");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Credenciales Incorrectas";
+            }
+            return View();
+        }
+    }
+}
---
+++ MvcSeguridadPersonalizada/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSeguridadPersonalizada.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSeguridadPersonalizada.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [AuthorizeUsers]
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
